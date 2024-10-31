949fa30173b3cb11a6413145b53bf6dfab5b4088
matutuam <tutebertuccio@hotmail.com>
2024-10-31 13:14:55 -0300

    agrega integración con una base de datos para preservar información

--- servidor.py
+++ servidor.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import pymysql
+from datetime import datetime
 
 class Servidor:
 
@@ -10,16 +12,26 @@ class Servidor:
         self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.servidor.bind((self.host, self.port))
         self.servidor.listen()
+
         print(f"\033[94m[SERVIDOR]: Corriendo en {self.host}:{self.port}.\033[0m")
 
         self.clientes = []
         self.nicknames = []
 
+        self.db = pymysql.connect(
+            host = "127.0.0.1",
+            user = "root",
+            password = "",
+            database = "chat_db"
+        )
+        self.cursor = self.db.cursor()
+        print("\033[92m[SERVIDOR]: Conexión a la base de datos exitosa.\033[0m")
+
     def transmitirMensaje(self, mensaje, _cliente):
-        mensaje_decodificado = mensaje.decode("utf-8")
-        
-        if "@" in mensaje_decodificado:
-            partes = mensaje_decodificado.split()
+        mensajeDecodificado = mensaje.decode("utf-8")
+
+        if "@" in mensajeDecodificado:
+            partes = mensajeDecodificado.split()
             nombreDestino = partes[1][1:]
 
             if nombreDestino in self.nicknames:
@@ -83,19 +95,40 @@ class Servidor:
         while True:
             cliente, address = self.servidor.accept()
             cliente.send("@nickname".encode('utf-8'))
-            username = cliente.recv(self.BUFFER_SIZE).decode('utf-8')
+            nickname = cliente.recv(self.BUFFER_SIZE).decode('utf-8')
 
-            self.clientes.append(cliente)
-            self.nicknames.append(username)
+            estadoUsuario = self.verificarUsuario(nickname)
 
-            print(f"\033[92m[SERVIDOR]: {username} está conectado con {str(address)}.\033[0m")
+            if estadoUsuario == "Conectado":
+                cliente.send(f"\033[91m[SERVIDOR]: El usuario '{nickname}' ya está conectado desde otra consola.\033[0m".encode("utf-8"))
+                cliente.close()
+            else:
+                self.clientes.append(cliente)
+                self.nicknames.append(nickname)
+                print(f"\033[92m[SERVIDOR]: {nickname} está conectado con {str(address)}.\033[0m")
 
-            message = f"\033[92m[SERVIDOR]: {username} se unió a ChatApp.\033[0m".encode('utf-8')
-            self.transmitirMensaje(message, cliente)
-            cliente.send("Conectado al servidor.".encode('utf-8'))
+                mensaje = f"\033[92m[SERVIDOR]: {nickname} se unió a ChatApp.\033[0m".encode('utf-8')
+                self.transmitirMensaje(mensaje, cliente)
+                cliente.send("Conectado al servidor.".encode('utf-8'))
 
-            thread = threading.Thread(target=self.manejarMensaje, args=(cliente,))
-            thread.start()
+                thread = threading.Thread(target=self.manejarMensaje, args=(cliente,))
+                thread.start()
+
+    def verificarUsuario(self, nickname):
+        self.cursor.execute("SELECT * FROM usuarios WHERE nickname = %s", (nickname,))
+        usuario = self.cursor.fetchone()
+
+        if usuario:
+            if nickname in self.nicknames:
+                return "Conectado"
+            else:
+                self.cursor.execute("UPDATE usuarios SET connected_at = %s WHERE nickname = %s", (datetime.now(), nickname))
+                self.db.commit()
+                return "Existe"
+        else:
+            self.cursor.execute("INSERT INTO usuarios (nickname, connected_at) VALUES (%s, %s)", (nickname, datetime.now()))
+            self.db.commit()
+            return "Nuevo"
 
     def iniciar(self):
         self.recibirConexion()
